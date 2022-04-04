1fba231400aabaa4bd7cab74056b4d070fdf9c6d
opurushothaman <omprakash.purushothaman@rntbci.com>
2022-04-04 17:49:49 +0530

    env port added

--- auditSite.ts
+++ auditSite.ts
@@ -3,7 +3,7 @@ const express = require('express');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/co2', (req, res) => {
 let c02grams : any = '';
