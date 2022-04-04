3f9a6d3659c4bff7f62c6eaeac1730364ba01dc0
opurushothaman <omprakash.purushothaman@rntbci.com>
2022-04-04 18:40:15 +0530

    try catch enabled

--- auditSite.ts
+++ auditSite.ts
@@ -6,21 +6,34 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/co2', (req, res) => {
-let c02grams : any = '';
-let lighthouse = new LightHouseWrapper();
+    let c02grams: any = '';
+    let lighthouse = new LightHouseWrapper();
 
-(async () => {
-   let urlVal:string = req.query.url;
-    console.log('url'+urlVal);
-    let co2g = await lighthouse.auditSite(urlVal);
-    console.log('co2g'+co2g);
-    c02grams = co2g;
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:58077');
-    res. setHeader('Content-Type', 'application/json');
-    res.send(co2g.toString());
-})();
+    (async () => {
+        try {
+            let urlVal: string = req.query.url || '';
+            if (urlVal) {
+                console.log('url' + urlVal);
+                let co2g = await lighthouse.auditSite(urlVal);
+                console.log('co2g' + co2g);
+                c02grams = co2g;
+                // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:58077');
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).send(co2g.toString());
+            }
+            else {
+                console.log('url not found');
+                res.setHeader('Content-Type', 'application/json');
+                res.status(500).send('url not found');
+            }
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+    })();
     //res.send(c02grams.toString() + 'g');
-   // res.send('Hello World, from express');
+    // res.send('Hello World, from express');
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
